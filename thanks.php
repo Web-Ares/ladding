652d4d4b3554c8421d521d5707218cc3c1a0bf0b
alex <alexnikol@e-mail.ua>
2015-11-11 09:15:05 +0200

    first

--- thanks.php
+++ thanks.php
@@ -1,3 +1,23 @@
+<?php
+if (!empty($_GET)&&!empty($_GET['send_email'])){
+    if ($_GET['send_email']=='true'){
+        $send='
+                <!--feedback__title-->
+                <h2 class="feedback-title">אנו נחזור אליך בהקדם<br>פנייתך נשלחה בהצלחה</h2>
+                <!--/feedback__title-->';
+    }
+    else{
+        $send='
+                <!--feedback__title-->
+                <h2 class="feedback-title">Try again later</h2>
+                <!--/feedback__title-->';
+
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <html lang="he" dir="rtl">
@@ -24,9 +44,7 @@
             <!--site__content__feedback-->
             <div class="site__content__feedback">
 
-                <!--feedback__title-->
-                <h2 class="feedback-title">אנו נחזור אליך בהקדם<br>פנייתך נשלחה בהצלחה</h2>
-                <!--/feedback__title-->
+              <?php echo $send; ?>
 
             </div>
             <!--/site__content__feedback-->
